133b13c93fa52966f630566b3aa369e8f79c4549
YashVar304 <yash21153028@akgec.ac.in>
2024-06-09 15:48:03 +0530

    responsive

--- src/App.jsx
+++ src/App.jsx
@@ -1,39 +1,31 @@
-// Importing necessary dependencies and components
 import { useEffect, useState } from "react";
 import { useSelector, useDispatch } from "react-redux";
-import { CiLocationOn } from "react-icons/ci";
+import { CiLocationOn, CiSearch } from "react-icons/ci";
 import { LiaTemperatureLowSolid } from "react-icons/lia";
 import { IoEyeOutline } from "react-icons/io5";
-import { LuDroplet, LuDroplets } from "react-icons/lu";
+import { LuDroplet } from "react-icons/lu";
 import useWeather from "./customhook/useWeather";
 import { Gauge } from "lucide-react";
-import { CiSearch } from "react-icons/ci";
-import { FaToggleOff, FaToggleOn } from "react-icons/fa";
+import { FaToggleOff, FaToggleOn, FaWind } from "react-icons/fa";
+import { FiSunrise, FiSunset } from "react-icons/fi";
+import { IoIosAdd } from "react-icons/io";
 import InfoCard from "./components/InfoCard";
 import AddCityCard from "./components/AddCityCard";
-import { FaWind } from "react-icons/fa";
-import { FiSunrise, FiSunset } from "react-icons/fi";
 import { toggleTheme } from "./features/themeSlice";
 import { addCity, updateCity } from "./features/citySlice";
-import { IoIosAdd } from "react-icons/io";
 
 function App() {
-  // State variables
   const [searchedCity, setSearchedCity] = useState("");
   const [searchAddCity, setSearchAddCity] = useState("");
   const [time, setTime] = useState("");
 
-  // Redux state variables
   const city = useSelector((state) => state.city.city);
-  let cities = useSelector((state) => state.city.cityArray);
+  const cities = useSelector((state) => state.city.cityArray);
   const theme = useSelector((state) => state.theme.theme);
   const dispatch = useDispatch();
 
-  // Custom hook for fetching weather data
   const weather = useWeather({ city });
-  const date = new Date();
 
-  // Effect to update time every second
   useEffect(() => {
     const interval = setInterval(() => {
       setTime(new Date().toLocaleTimeString());
@@ -41,34 +33,30 @@ function App() {
     return () => clearInterval(interval);
   }, []);
 
-  // Function to handle theme toggling
   const themeHandler = () => {
     dispatch(toggleTheme());
   };
 
-  // Effect to set theme class on HTML element whenever theme changes
   useEffect(() => {
     document.querySelector("html").classList.remove("dark", "light");
     document.querySelector("html").classList.add(theme);
   }, [theme]);
 
-  // Render JSX
   return (
     <div className="min-h-screen p-3 md:p-10 bg-gradient-to-br to-blue-500 from-white dark:from-gray-300 dark:to-black bg-cover bg-center">
-      <div className="flex flex-wrap flex-col-responsive w-full h-full dark:bg-opacity-50 rounded-2xl dark:bg-slate-50 bg-blue-500 bg-opacity-50">
-        <div className=" w-full md:w-[40%]">
+      <div className="flex flex-wrap w-full h-full dark:bg-opacity-50 rounded-2xl dark:bg-slate-50 bg-blue-500 bg-opacity-50">
+        <div className="w-full md:w-2/5">
           {weather.loading && (
-            <p className="text-white dark:text-black ">Loading...</p>
+            <p className="text-white dark:text-black">Loading...</p>
           )}
           <div className="flex flex-col p-5 md:p-10 h-full">
-            {/* Search bar for location */}
             <div className="flex justify-between items-center">
-              <div className="flex justify-center items-center bg-white px-3 py-1 dark:bg-black dark:bg-opacity-70 rounded-3xl">
+              <div className="flex items-center bg-white px-3 py-1 dark:bg-black dark:bg-opacity-70 rounded-3xl w-full">
                 <CiLocationOn size={25} className="dark:text-gray-400 mr-2" />
                 <input
                   type="text"
                   placeholder="Enter your location"
-                  className="w-full rounded-lg outline-none bg-transparent dark:text-gray-400 "
+                  className="w-full rounded-lg outline-none bg-transparent dark:text-gray-400"
                   value={searchedCity}
                   onChange={(e) => setSearchedCity(e.target.value)}
                 />
@@ -79,7 +67,6 @@ function App() {
                   <CiSearch className="text-xl dark:text-gray-400" />
                 </button>
               </div>
-              {/* Theme toggle button */}
               {theme === "light" ? (
                 <FaToggleOff
                   size={30}
@@ -89,14 +76,13 @@ function App() {
               ) : (
                 <FaToggleOn
                   size={30}
-                  className=" cursor-pointer ml-2"
+                  className="cursor-pointer ml-2"
                   onClick={themeHandler}
                 />
               )}
             </div>
-            {/* Display date and time */}
             <div className="w-full h-full mt-5">
-              <div className="flex  justify-between px-5 items-center">
+              <div className="flex justify-between px-5 items-center">
                 <p className="text-white text-md md:text-lg dark:text-gray-700 font-mono">
                   {Date().slice(0, 15)}
                 </p>
@@ -104,13 +90,12 @@ function App() {
                   {time.slice(0, 8)}
                 </p>
               </div>
-              {/* Display weather information */}
               {weather.error ? (
                 <div className="flex justify-center items-center h-56">
-                  <p className="text-white dark:text-black ">City not found</p>
+                  <p className="text-white dark:text-black">City not found</p>
                 </div>
               ) : (
-                <div className="flex justify-evenly items-center">
+                <div className="flex flex-col md:flex-row justify-evenly items-center">
                   <div className="flex justify-center items-center">
                     <img
                       src={`http://openweathermap.org/img/wn/${
@@ -120,24 +105,24 @@ function App() {
                         weather.data.weather[0].icon
                       }@2x.png`}
                       alt="weather"
-                      className="w-40 h-40"
+                      className="w-32 h-32 md:w-40 md:h-40"
                     />
                   </div>
-                  <div className="flex flex-col justify-center items-center py-10">
-                    <h1 className="text-white text-5xl md:text-6xl font-ubuntu dark:text-gray-700">
+                  <div className="flex flex-col justify-center items-center py-4">
+                    <h1 className="text-white text-4xl md:text-6xl font-ubuntu dark:text-gray-700">
                       {weather &&
                         weather.data &&
                         weather.data.main &&
                         Math.floor(weather.data.main.temp - 273)}
                       °
                     </h1>
-                    <h3 className="text-white text-xl mt-3 mr-5 font-thin font-ubuntu dark:text-gray-700">
+                    <h3 className="text-white text-lg md:text-xl mt-3 font-thin font-ubuntu dark:text-gray-700">
                       {weather &&
                         weather.data &&
                         weather.data.weather &&
                         weather.data.weather[0].main}
                     </h3>
-                    <h1 className="text-white text-xl mt-3 mr-5 font-ubuntu dark:text-gray-700">
+                    <h1 className="text-white text-lg md:text-xl mt-3 font-ubuntu dark:text-gray-700">
                       {weather &&
                         weather.data &&
                         weather.data.name &&
@@ -146,8 +131,7 @@ function App() {
                   </div>
                 </div>
               )}
-              {/* Display additional weather information */}
-              <div className="flex flex-col gap-2">
+              <div className="flex flex-col gap-2 mt-5">
                 <InfoCard
                   icon={<LiaTemperatureLowSolid className="mr-2" />}
                   name="Feels Like"
@@ -191,14 +175,15 @@ function App() {
             </div>
           </div>
         </div>
-        <div className="w-full md:w-[60%]">
+        <div className="w-full md:w-3/5">
           <div className="p-5 md:p-10 flex flex-col-reverse md:flex-col">
-            {/* Add City section */}
             <div className="bg-black bg-opacity-30 rounded-2xl p-5 mt-2">
               <div className="flex justify-between">
-                <h1 className="text-white text-2xl mr-2 font-mono">Cities</h1>
-                <div className="flex justify-between items-center">
-                  <div className="flex justify-center items-center bg-white px-3 py-1 dark:bg-black dark:bg-opacity-70 rounded-3xl">
+                <h1 className="text-white text-xl md:text-2xl mr-2 font-mono">
+                  Cities
+                </h1>
+                <div className="flex items-center">
+                  <div className="flex items-center bg-white px-3 py-1 dark:bg-black dark:bg-opacity-70 rounded-3xl w-full">
                     <CiLocationOn
                       size={25}
                       className="dark:text-gray-400 mr-2"
@@ -217,29 +202,30 @@ function App() {
                         setSearchAddCity("");
                       }}
                     >
-                      <IoIosAdd className="text-xl dark:text-gray-400 " />
+                      <IoIosAdd className="text-xl dark:text-gray-400" />
                     </button>
                   </div>
                 </div>
               </div>
-              {/* List of added cities */}
-              <div className="scroll-container">
+              <div className="scroll-container max-h-96 overflow-y-auto">
                 {cities &&
                   cities.map((city, index) => (
                     <AddCityCard key={index} city={city} />
                   ))}
               </div>
             </div>
-            {/* Additional weather information */}
-            <div className="flex md:mt-4 flex-wrap gap-3 bg-opacity-30 rounded-2xl">
-              {/* Wind information */}
-              <div className="w-full md:w-[48%] flex bg-opacity-70 bg-black p-5 rounded-2xl ">
-                <div className="flex flex-col justify-between flex-[1] ">
-                  <h1 className="text-white text-xl mb-5 font-mono">Wind</h1>
+            <div className="flex flex-wrap gap-3 bg-opacity-30 rounded-2xl mt-4">
+              <div className="w-full md:w-48% flex bg-opacity-70 bg-black p-5 rounded-2xl">
+                <div className="flex flex-col justify-between flex-1">
+                  <h1 className="text-white text-lg md:text-xl mb-5 font-mono">
+                    Wind
+                  </h1>
                   <div className="flex flex-col justify-between">
-                    <div className="flex flex-col ">
-                      <h1 className="text-white text-lg  font-ubuntu">Speed</h1>
-                      <h1 className="text-white text-md font-ubuntu">
+                    <div className="flex flex-col">
+                      <h1 className="text-white text-md md:text-lg font-ubuntu">
+                        Speed
+                      </h1>
+                      <h1 className="text-white text-sm md:text-md font-ubuntu">
                         {weather &&
                           weather.data &&
                           weather.data.wind &&
@@ -247,10 +233,12 @@ function App() {
                         m/s
                       </h1>
                     </div>
-                    <hr />
-                    <div className="flex flex-col ">
-                      <h1 className="text-white text-lg font-ubuntu">Degree</h1>
-                      <h1 className="text-white text-md font-ubuntu">
+                    <hr className="my-2" />
+                    <div className="flex flex-col">
+                      <h1 className="text-white text-md md:text-lg font-ubuntu">
+                        Degree
+                      </h1>
+                      <h1 className="text-white text-sm md:text-md font-ubuntu">
                         {weather &&
                           weather.data &&
                           weather.data.wind &&
@@ -260,23 +248,22 @@ function App() {
                     </div>
                   </div>
                 </div>
-                <div className="flex justify-center items-center flex-[2]">
+                <div className="flex justify-center items-center flex-2">
                   <FaWind size={60} className="text-white" />
-                </div>{" "}
+                </div>
               </div>
-              {/* Sunrise & Sunset information */}
-              <div className="w-full md:w-[48%]  flex bg-opacity-70 bg-black p-5 rounded-2xl ">
-                <div className="flex flex-col w-full ">
-                  <h1 className="text-white text-xl mb-5 font-mono">
+              <div className="w-full md:w-48% flex bg-opacity-70 bg-black p-5 rounded-2xl">
+                <div className="flex flex-col w-full">
+                  <h1 className="text-white text-lg md:text-xl mb-5 font-mono">
                     Sunrise & Sunset
                   </h1>
                   <div className="flex flex-col justify-between w-full">
-                    <div className="flex justify-between items-center ">
+                    <div className="flex justify-between items-center">
                       <div className="flex flex-col">
-                        <p className="text-white text-lg font-ubuntu">
+                        <p className="text-white text-md md:text-lg font-ubuntu">
                           Sunrise
                         </p>
-                        <p className="text-white text-md font-ubuntu">
+                        <p className="text-white text-sm md:text-md font-ubuntu">
                           {weather &&
                             weather.data &&
                             weather.data.sys &&
@@ -285,13 +272,15 @@ function App() {
                             ).toLocaleTimeString()}
                         </p>
                       </div>
-                      <FiSunrise className=" text-white mr-5" size={40} />
+                      <FiSunrise className="text-white mr-5" size={40} />
                     </div>
-                    <hr />
-                    <div className="flex justify-between items-center ">
-                      <div className="flex flex-col ">
-                        <p className="text-white text-lg font-ubuntu">Sunset</p>
-                        <p className="text-white text-md font-ubuntu">
+                    <hr className="my-2" />
+                    <div className="flex justify-between items-center">
+                      <div className="flex flex-col">
+                        <p className="text-white text-md md:text-lg font-ubuntu">
+                          Sunset
+                        </p>
+                        <p className="text-white text-sm md:text-md font-ubuntu">
                           {weather &&
                             weather.data &&
                             weather.data.sys &&
@@ -300,7 +289,7 @@ function App() {
                             ).toLocaleTimeString()}
                         </p>
                       </div>
-                      <FiSunset className=" text-white mr-5" size={40} />
+                      <FiSunset className="text-white mr-5" size={40} />
                     </div>
                   </div>
                 </div>
